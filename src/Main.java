d7ec06958117a1f9524339b4fc3da108ff5f2bee
Raymond <r.danjou@student.tudelft.nl>
2018-12-22 13:49:29 +0100

    Made merge sort, but then improved it by sorting small sets using insertion sort on a part of the array so we dont need to copy the sub array

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
+import sorting.Sorter;
+
+import java.util.Random;
+
 public class Main {
 
+    //Stuff to check the performance
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] randomOrdered = new int[10000000];
+        Random rand = new Random();
+
+        System.out.println("STARTING SORT");
+
+        double totalTime = 0;
+
+        //for(int y = 0; y < 1;y++) {
+        //    for(int x = 0; x < randomOrdered.length;x++){
+        //        randomOrdered[x] = rand.nextInt((100000 - 1) + 1) + 1;
+        //    }
+        //    long startTime = System.currentTimeMillis();
+//
+        //    Sorter.mergeSort(randomOrdered);
+        //    long endTime = System.currentTimeMillis();
+        //    double finalTime = (endTime - startTime) / 1000.0;
+        //    totalTime += finalTime;
+        //}
+
+        for(int x = 0; x < randomOrdered.length;x++){
+            randomOrdered[x] = rand.nextInt((100 - 1) + 1) + 1;
+        }
+
+        long startTime = System.currentTimeMillis();
+
+        Sorter.mergeSort(randomOrdered);
+        long endTime = System.currentTimeMillis();
+        double finalTime = (endTime - startTime) / 1000.0;
+
+        //for(int x = 0; x < randomOrdered.length;x++){
+        //    System.out.println(randomOrdered[x]);
+        //}
+
+        System.out.println(finalTime);
     }
 }
--- src/sorting/Sorter.java
+++ src/sorting/Sorter.java
@@ -4,8 +4,81 @@ import java.util.ArrayList;
 
 public class Sorter {
 
-    public Sorter(ArrayList<?> theList){
+
+    //1M numbers in 0.17 seconds
+    public static void mergeSort(int[] array) {
+        mergeDivider(array,0,array.length-1);
+    }
+
+    private static void mergeDivider(int[] array, int lowBound, int highBound) {
+        if(lowBound < highBound) {
+            int middle = (lowBound + highBound)/2;
+
+            if((highBound - lowBound) <= 5){
+                mergeSortInsertionSortSpeedUp(array,lowBound,highBound);
+            } else {
+                mergeDivider(array, lowBound, middle);
+                mergeDivider(array, middle + 1, highBound);
+
+                if(array[middle] > array[middle + 1]){
+                    mergeSortMerger(array, lowBound, middle, highBound);
+                }
+            }
+        }
+    }
+
+    //Merge method, efficiency is O(n)
+    private static void mergeSortMerger(int[] array, int lowBound, int middle, int highBound) {
+
+        int left_index = lowBound, right_index = middle + 1,temp_index = 0;
+        int[] temp_holder = new int[highBound - lowBound + 1];
+
+        while(left_index <= middle && right_index <= highBound){
+            if(array[left_index] < array[right_index]){
+                temp_holder[temp_index++] = array[left_index++];
+            } else {
+                temp_holder[temp_index++] = array[right_index++];
+            }
+        }
+
+        while(left_index <= middle){
+            temp_holder[temp_index++] = array[left_index++];
+        }
+        while(right_index <= highBound){
+            temp_holder[temp_index++] = array[right_index++];
+        }
+
+        for(int x = lowBound, k = 0; x <=highBound;x++,k++){
+            array[x] = temp_holder[k];
+        }
+    }
+
+    private static void mergeSortInsertionSortSpeedUp(int[] array, int left, int right){
+        for(int x = left; x <= right;x++){
+            int temp = array[x];
+            int before = x - 1;
+            while(before >= left && array[before] > temp){
+                array[before+1] = array[before];
+                before = before - 1;
+            }
+            array[before+1] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] array){
+        int array_length = array.length;
+        for(int x = 0; x < array_length;x++){
+            int current = array[x];
+            int before = x - 1;
+            while(before >= 0 && array[before] > current){
+                array[before+1] = array[before];
+                before = before - 1;
+            }
+            array[before+1] = current;
+        }
+    }
+
+    private static void insertionSorter(){
 
     }
-    
 }
